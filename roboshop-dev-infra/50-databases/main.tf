fca97213985f36495c044d9f14a9c318dfcfd31b
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-11-07 17:05:54 +0530

    changes

--- roboshop-dev-infra/50-databases/main.tf
+++ roboshop-dev-infra/50-databases/main.tf
@@ -12,20 +12,8 @@ resource "aws_instance" "mongodb" {
     )
 
     provisioner "remote-exec" {
-         
-      
-    }
-}
-
-resource "aws_instance" "bastion" {
-    ami = local.ami_id
-    subnet_id   = split("," , data.aws_ssm_parameter.public_subnet.value)[0]
-    instance_type = "t3.micro"  
-    vpc_security_group_ids = [data.aws_ssm_parameter.bastion_sg_id.value]
-    
-}
-
-output "public_id" {
-
-  value = aws_instance.bastion.public_ip
+        inline = [ 
+            echo "Hello"
+         ]
+          }
 }
