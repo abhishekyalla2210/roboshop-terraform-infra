a305909c766dfa0bd799979d78f98f71b1a31c3d
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-11-07 16:58:08 +0530

    changes

---
+++ roboshop-dev-infra/50-databases/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    type = string
+    default = "roboshop"
+}
+
+variable "environment_name" {
+    type = string
+    default = "dev"
+  
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+  
+}
