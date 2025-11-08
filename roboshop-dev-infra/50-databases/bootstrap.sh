dec6b51af50a67f75f76becc10886e32256ea3c1
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-11-08 19:15:00 +0530

    changes

--- roboshop-dev-infra/50-databases/bootstrap.sh
+++ roboshop-dev-infra/50-databases/bootstrap.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+# component=$1
+# dnf install ansible -y
+# ansible-pull -U https://github.com/abhishekyalla2210/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+
+
 component=$1
-dnf install ansible -y
-ansible-pull -U https://github.com/abhishekyalla2210/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+REPO_URL=https://github.com/abhishekyalla2210/ansible-roboshop-roles-tf.git
+
+REPO_DIR=/opt/roboshop/ansible
+ANSIBLE_DIR=ansible-roboshop-roles-tf
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop
+touch ansible.log
+
+cd $REPO_DIR
+
+if [ -d $ANSIBLE_DIR ]; then
+    cd $ANSIBLE_DIR
+    git pull
+else
+    git clone
+    cd $ANSIBLE_DIR
+fi
+    ansible-playbook -e component=$component main.yaml
