ac031bb712e417d7943292be045774861299d183
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-11-08 12:08:31 +0530

    my changes

--- roboshop-dev-infra/50-databases/bootstrap.sh
+++ roboshop-dev-infra/50-databases/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dnf install ansible -y
-ansible-pull -U https://github.com/abhishekyalla2210/terraforms.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/abhishekyalla2210/terraforms.git /roboshop-dev-infra -e component=mongodb main.yaml
