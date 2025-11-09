1e8d622129aed78ef112be90eddc73767237b3b4
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-11-09 22:25:02 +0530

    changes

--- roboshop-dev-infra/50-databases/locals.tf
+++ roboshop-dev-infra/50-databases/locals.tf
@@ -6,6 +6,8 @@ locals {
   redis_sg_id = [data.aws_ssm_parameter.redis_sg_id.value]
   rabbitmq_sg_id = [data.aws_ssm_parameter.rabbitmq_sg_id.value]
   mysql_sg_id = [data.aws_ssm_parameter.mysql_sg_id.value]
+    catalogue_sg_id = [data.aws_ssm_parameter.catalogue_sg_id.value]
+
   # private_ip = [aws_instance[var.[count.index]].private_ip]
   # private_ip = [for name in var.route_names : name => [aws_instance.[name].private_ip]]
 
@@ -28,7 +30,7 @@ locals {
     redis    = aws_instance.redis.private_ip
     mysql    = aws_instance.mysql.private_ip
     # catalogue = aws_instance.catalogue.private_ip
-  #   catalogue    = aws_instance.catalogue.private_ip
+    catalogue    = aws_instance.catalogue.private_ip
 
    }
 }
