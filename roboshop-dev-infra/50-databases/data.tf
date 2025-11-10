9613d8ee7ab56685d4445e9371f350b1e7f4ae98
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-11-10 07:20:25 +0530

    changes

--- roboshop-dev-infra/50-databases/data.tf
+++ roboshop-dev-infra/50-databases/data.tf
@@ -47,16 +47,3 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
   name = "/${local.common_name}/catalogue"
   }
 
- data "aws_instance" "catalogue" {
-  instance_id = "i-instanceid"
-
-  filter {
-    name   = "image-id"
-    values = ["ami-xxxxxxxx"]
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = ["instance-name-tag"]
-  }
-}
