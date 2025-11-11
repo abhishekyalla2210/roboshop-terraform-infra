c5189c3978eb4b31f69e09e327999873588ab496
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-11-11 16:47:23 +0530

    changes

--- roboshop-dev-infra/60-catalogue/main.tf
+++ roboshop-dev-infra/60-catalogue/main.tf
@@ -41,17 +41,17 @@ resource "terraform_data" "catalogue" {
 
 
 
-# resource "aws_ec2_instance_state" "catalogue" {
-#   instance_id = aws_instance.catalogue.id
-#   state       = "stopped"
-#   depends_on = [ terraform_data.catalogue ]
-# }
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+  depends_on = [ terraform_data.catalogue ]
+}
 
-# resource "aws_ami_from_instance" "catalogue" {
-#   source_instance_id = aws_instance.catalogue.id
-#   name               = "${local.common_name}-catalogue-ami"
-#   depends_on = [ aws_ec2_instance_state.catalogue ]
-#   }
+resource "aws_ami_from_instance" "catalogue" {
+  source_instance_id = aws_instance.catalogue.id
+  name               = "${local.common_name}-catalogue-ami"
+  depends_on = [ aws_ec2_instance_state.catalogue ]
+  }
 
 
 resource "aws_lb_target_group" "catalogue" {
@@ -74,110 +74,110 @@ resource "aws_lb_target_group" "catalogue" {
 }
 
 
-# resource "aws_launch_template" "catalogue" {
-#   name = "${local.common_name}-catalogue"
-#   image_id = aws_ami_from_instance.catalogue.id
+resource "aws_launch_template" "catalogue" {
+  name = "${local.common_name}-catalogue"
+  image_id = aws_ami_from_instance.catalogue.id
 
-#   instance_initiated_shutdown_behavior = "terminate"
-#   instance_type = "t3.micro"
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "t3.micro"
 
-#   vpc_security_group_ids = local.catalogue_sg_id
+  vpc_security_group_ids = local.catalogue_sg_id
 
-#   # when we run terraform apply again, a new version will be created with new AMI ID
-#   update_default_version = true
+  # when we run terraform apply again, a new version will be created with new AMI ID
+  update_default_version = true
 
-#   # tags attached to the instance
-#   tag_specifications {
-#     resource_type = "instance"
+  # tags attached to the instance
+  tag_specifications {
+    resource_type = "instance"
 
-#     tags = merge(
-#       local.common_tags,
-#       {
-#         Name = "${local.common_name}-catalogue"
-#       }
-#     )
-#   }
-#  # tags attached to the volume created by instance
-#   tag_specifications {
-#     resource_type = "volume"
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name}-catalogue"
+      }
+    )
+  }
+ # tags attached to the volume created by instance
+  tag_specifications {
+    resource_type = "volume"
 
-#     tags = merge(
-#       local.common_tags,
-#       {
-#         Name = "${local.common_name}-catalogue"
-#       }
-#     )
-#   }
-#    # tags attached to the launch template
-#   tags = merge(
-#       local.common_tags,
-#       {
-#         Name = "${local.common_name}-catalogue"
-#       }
-#   )
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name}-catalogue"
+      }
+    )
+  }
+   # tags attached to the launch template
+  tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name}-catalogue"
+      }
+  )
 
-# }
+}
 
   
-# resource "aws_autoscaling_group" "catalogue" {
-#   name                      = "${local.common_name}-catalogue"
-#   max_size                  = 10
-#   min_size                  = 1
-#   health_check_grace_period = 100
-#   health_check_type         = "ELB"
-#   desired_capacity          = 1
-#   force_delete              = false
-#   launch_template {
-#     id      = aws_launch_template.catalogue.id
-#     version = aws_launch_template.catalogue.latest_version
-#   }
-#   vpc_zone_identifier       = local.private_subnet_ids
-#   target_group_arns = [aws_lb_target_group.catalogue.arn]
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${local.common_name}-catalogue"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 100
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = false
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = aws_launch_template.catalogue.latest_version
+  }
+  vpc_zone_identifier       = local.private_subnet_ids
+  target_group_arns = [aws_lb_target_group.catalogue.arn]
 
-#   instance_refresh {
-#     strategy = "Rolling"
-#     preferences {
-#       min_healthy_percentage = 50 # atleast 50% of the instances should be up and running
-#     }
-#     triggers = ["launch_template"]
-#   }
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50 # atleast 50% of the instances should be up and running
+    }
+    triggers = ["launch_template"]
+  }
   
-#   dynamic "tag" {  # we will get the iterator with name as tag
-#     for_each = merge(
-#       local.common_tags,
-#       {
-#         Name = "${local.common_name}-catalogue"
-#       }
-#     )
-#     content {
-#       key                 = tag.key
-#       value               = tag.value
-#       propagate_at_launch = true
-#     }
-#   }
+  dynamic "tag" {  # we will get the iterator with name as tag
+    for_each = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name}-catalogue"
+      }
+    )
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
 
-#   timeouts {
-#     delete = "15m"
-#   }
+  timeouts {
+    delete = "15m"
+  }
 
-# }
+}
 
  
 
 
-# resource "aws_autoscaling_policy" "catalogue" {
-#   autoscaling_group_name = aws_autoscaling_group.catalogue.name
-#   name                   = "${local.common_name}-catalogue"
-#   policy_type            = "TargetTrackingScaling"
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  name                   = "${local.common_name}-catalogue"
+  policy_type            = "TargetTrackingScaling"
 
-#   target_tracking_configuration {
-#     predefined_metric_specification {
-#       predefined_metric_type = "ASGAverageCPUUtilization"
-#     }
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
 
-#     target_value = 75.0
-#   }
-# }
+    target_value = 75.0
+  }
+}
 
 resource "aws_lb_listener_rule" "catalogue" {
   listener_arn = local.backend_alb_listener_arn
