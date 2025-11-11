3baaed53f445ddd10bad0918785007d084cd5758
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-11-11 14:51:20 +0530

    changes

--- roboshop-dev-infra/60-catalogue/main.tf
+++ roboshop-dev-infra/60-catalogue/main.tf
@@ -54,69 +54,69 @@ resource "aws_ami_from_instance" "catalogue" {
   }
 
 
-# resource "aws_lb_target_group" "catalogue" {
-#   name     = "${local.common_name}-catalogue"
-#   port     = 8080
-#   protocol = "HTTP"
-#   vpc_id   = local.vpc_id
-#   deregistration_delay = 60 # waiting period before deleting the instance
+resource "aws_lb_target_group" "catalogue" {
+  name     = "${local.common_name}-catalogue"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 60 # waiting period before deleting the instance
 
-#   health_check {
-#     healthy_threshold = 2
-#     interval = 10
-#     matcher = "200-299"
-#     path = "/health"
-#     port = 8080
-#     protocol = "HTTP"
-#     timeout = 2
-#     unhealthy_threshold = 2
-#   }
-# }
+  health_check {
+    healthy_threshold = 2
+    interval = 10
+    matcher = "200-299"
+    path = "/health"
+    port = 8080
+    protocol = "HTTP"
+    timeout = 2
+    unhealthy_threshold = 2
+  }
+}
 
 
-# resource "aws_launch_template" "catalogue" {
-#   name = "${local.common_name}-catalogue"
-#   image_id = aws_ami_from_instance.catalogue.id
+resource "aws_launch_template" "catalogue" {
+  name = "${local.common_name}-catalogue"
+  image_id = aws_ami_from_instance.catalogue.id
 
-#   instance_initiated_shutdown_behavior = "terminate"
-#   instance_type = "t3.micro"
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "t3.micro"
 
-#   vpc_security_group_ids = [local.catalogue_sg_id]
+  vpc_security_group_ids = [local.catalogue_sg_id]
 
-#   # when we run terraform apply again, a new version will be created with new AMI ID
-#   update_default_version = true
+  # when we run terraform apply again, a new version will be created with new AMI ID
+  update_default_version = true
 
-#   # tags attached to the instance
-#   tag_specifications {
-#     resource_type = "instance"
+  # tags attached to the instance
+  tag_specifications {
+    resource_type = "instance"
 
-#     tags = merge(
-#       local.common_tags,
-#       {
-#         Name = "${local.common_name}-catalogue"
-#       }
-#     )
-#   }
-#  # tags attached to the volume created by instance
-#   tag_specifications {
-#     resource_type = "volume"
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name}-catalogue"
+      }
+    )
+  }
+ # tags attached to the volume created by instance
+  tag_specifications {
+    resource_type = "volume"
 
-#     tags = merge(
-#       local.common_tags,
-#       {
-#         Name = "${local.common_name}-catalogue"
-#       }
-#     )
-#   }
-#    # tags attached to the launch template
-#   tags = merge(
-#       local.common_tags,
-#       {
-#         Name = "${local.common_name}-catalogue"
-#       }
-#   )
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name}-catalogue"
+      }
+    )
+  }
+   # tags attached to the launch template
+  tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name}-catalogue"
+      }
+  )
 
-# }
+}
 
   
 # resource "aws_autoscaling_group" "catalogue" {
