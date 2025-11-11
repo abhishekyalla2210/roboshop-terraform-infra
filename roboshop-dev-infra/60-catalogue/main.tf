f3708c9ba1305ca6d63d97f3c8edd30e13791c79
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-11-11 17:57:21 +0530

    changes

--- roboshop-dev-infra/60-catalogue/main.tf
+++ roboshop-dev-infra/60-catalogue/main.tf
@@ -1,162 +1,173 @@
-###########################################################
-# EC2 Instance to Bake AMI
-###########################################################
+  
 resource "aws_instance" "catalogue" {
-  ami                    = var.ami_id
-  subnet_id              = local.subnet_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = local.catalogue_sg_id
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "${local.common_name}-catalogue"
-    }
-  )
+    ami = var.ami_id
+    subnet_id   = local.subnet_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = local.catalogue_sg_id
+     tags = merge(
+        local.common_tags,
+        {
+            Name = "${local.common_name}-catalogue"
+        }
+    )
 }
 
-###########################################################
-# Provision catalogue on EC2
-###########################################################
 resource "terraform_data" "catalogue" {
-  triggers_replace = [aws_instance.catalogue.id]
 
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = aws_instance.catalogue.private_ip
-  }
-
-  provisioner "file" {
-    source      = "catalogue.sh"
-    destination = "/tmp/catalogue.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo chmod +x /tmp/catalogue.sh",
-      "sudo sh /tmp/catalogue.sh catalogue"
+    triggers_replace = [
+        aws_instance.catalogue.id
     ]
+     connection {
+        type        = "ssh"
+        user        = "ec2-user"
+        password  = "DevOps321"
+        host        = aws_instance.catalogue.private_ip
   }
+      provisioner "file" {
+        source = "catalogue.sh"  
+        destination = "/tmp/catalogue.sh"
+      }
+     provisioner "remote-exec" { 
+     inline =   [
+        "sudo chmod +x /tmp/catalogue.sh",
+        "sudo sh /tmp/catalogue.sh catalogue"
+      ] 
+    }  
+    }
+
+
+
+  
+
+
+
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+  depends_on = [ terraform_data.catalogue ]
 }
 
-###########################################################
-# AMI from Running Instance (no stop!)
-###########################################################
 resource "aws_ami_from_instance" "catalogue" {
   source_instance_id = aws_instance.catalogue.id
   name               = "${local.common_name}-catalogue-ami"
-  depends_on         = [terraform_data.catalogue]
-}
+  depends_on = [ aws_ec2_instance_state.catalogue ]
+  }
 
-###########################################################
-# Load Balancer Target Group
-###########################################################
+
 resource "aws_lb_target_group" "catalogue" {
   name     = "${local.common_name}-catalogue"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   = local.vpc_id
+  vpc_id   =  data.aws_ssm_parameter.vpc_id.value
   deregistration_delay = 60
-
-  health_check {
+    
+     health_check {
+    path                = "/health"
+    port = 8080
+    protocol            = "HTTP"
+    matcher             = "200-299"
+    interval            = 10
+    timeout             = 2
     healthy_threshold   = 2
     unhealthy_threshold = 2
-    interval            = 10
-    timeout             = 5
-    matcher             = "200-299"
-    path                = "/health"
-    protocol            = "HTTP"
-    port                = 8080
   }
 }
 
-###########################################################
-# Launch Template for ASG
-###########################################################
+
 resource "aws_launch_template" "catalogue" {
   name = "${local.common_name}-catalogue"
-  image_id = aws_ami_from_instance.catalogue.id
+
 
   instance_initiated_shutdown_behavior = "terminate"
+  image_id = aws_ami_from_instance.catalogue.id
   instance_type = "t3.micro"
   vpc_security_group_ids = local.catalogue_sg_id
+ tag_specifications {
+   resource_type = "instance"
+ 
 
-  update_default_version = true
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = merge(local.common_tags, { Name = "${local.common_name}-catalogue" })
+ tags = merge(
+  local.common_tags,
+  {
+    Name = local.common_name
   }
-
+ ) 
+ }
   tag_specifications {
-    resource_type = "volume"
-    tags = merge(local.common_tags, { Name = "${local.common_name}-catalogue" })
+   resource_type = "volume"
+  tags = merge(
+  local.common_tags,
+  {
+    Name = "${local.common_name}-catalogue"
   }
-
-  tags = merge(local.common_tags, { Name = "${local.common_name}-catalogue" })
+ ) 
+  
+  }
 }
 
-###########################################################
-# Autoscaling Group
-###########################################################
+
+
 resource "aws_autoscaling_group" "catalogue" {
-  name                      = "${local.common_name}-catalogue"
+  name = "${local.common_name}-catalogue"
   max_size                  = 10
   min_size                  = 1
-  desired_capacity          = 1
   health_check_grace_period = 100
   health_check_type         = "ELB"
-  vpc_zone_identifier       = local.private_subnet_ids
-  target_group_arns         = [aws_lb_target_group.catalogue.arn]
-
+  desired_capacity          = 1
+  force_delete              = false
+  target_group_arns = [aws_lb_target_group.catalogue.arn]
+  depends_on = [ aws_lb_target_group.catalogue ]
   launch_template {
-    id      = aws_launch_template.catalogue.id
+    id = aws_launch_template.catalogue.id
     version = aws_launch_template.catalogue.latest_version
   }
+  vpc_zone_identifier       = local.private_subnet_id
 
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 50
-    }
-    triggers = ["launch_template"]
-  }
-
+  
   dynamic "tag" {
-    for_each = merge(local.common_tags, { Name = "${local.common_name}-catalogue" })
-    content {
-      key                 = tag.key
-      value               = tag.value
-      propagate_at_launch = true
-    }
+    for_each = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name}-catalogue"
+      }
+    )
+  
+
+  content {
+    key                 = tag.key
+    value               = tag.value
+    propagate_at_launch = true
   }
+  }
+
+  
+
+  
+
 
   timeouts {
     delete = "15m"
   }
+  
+  
 }
 
-###########################################################
-# Autoscaling Policy (CPU Scaling)
-###########################################################
+
 resource "aws_autoscaling_policy" "catalogue" {
   autoscaling_group_name = aws_autoscaling_group.catalogue.name
-  name                   = "${local.common_name}-catalogue"
-  policy_type            = "TargetTrackingScaling"
+  name = "${local.common_name}-catalogue"
+  policy_type = "TargetTrackingScaling"
 
   target_tracking_configuration {
     predefined_metric_specification {
       predefined_metric_type = "ASGAverageCPUUtilization"
     }
     target_value = 75.0
+    
   }
-}
+ }
 
-###########################################################
-# Listener Rule for Backend ALB
-###########################################################
+
 resource "aws_lb_listener_rule" "catalogue" {
   listener_arn = local.backend_alb_listener_arn
   priority     = 10
@@ -172,3 +183,24 @@ resource "aws_lb_listener_rule" "catalogue" {
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
