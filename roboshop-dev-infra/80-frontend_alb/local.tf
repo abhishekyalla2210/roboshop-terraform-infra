1c6121b0570f1bfe65741ff2939e1c859249fac8
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-11-12 15:42:10 +0530

    changes

--- roboshop-dev-infra/80-frontend_alb/local.tf
+++ roboshop-dev-infra/80-frontend_alb/local.tf
@@ -1,7 +1,5 @@
 locals {
   common_name_suffix = "${var.project_name}-${var.environment_name}"
-  backend_alb_sg_name = data.aws_ssm_parameter.sg_ids.value
-  private_subnet = split(",",data.aws_ssm_parameter.private_subnet.value)
   public_subnet = split(",",data.aws_ssm_parameter.public_subnet.value)
   frontend_alb_sg_name = data.aws_ssm_parameter.frontend_alb_sg_ids.value
   certificate_arn = data.aws_ssm_parameter.certificate_arn.value
