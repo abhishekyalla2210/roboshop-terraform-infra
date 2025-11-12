fa1cad1b308eff4d9517462c021581abd15e1ce6
abhishekyalla2210 <abhishekyalla7@gmail.com>
2025-11-12 15:23:51 +0530

    changes

--- roboshop-dev-infra/70-acm/parameters.tf
+++ roboshop-dev-infra/70-acm/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "roboshop_arn" {
     name = "/${var.project_name}-${var.environment}/roboshop_arn"
-    type = "string"
+    type = "String"
     value = aws_acm_certificate.roboshop.arn
 }
